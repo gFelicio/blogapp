b863b8ccadeeb6afd15c417e26c2d4d381a7e6f6
Gustavo Felicio <gustavo.a.felicio@gmail.com>
2026-02-20 23:11:53 -0300

    crud category

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // Módulos
     const express = require('express')
     const { engine } = require('express-handlebars')
+    const session = require('express-session')
+    const flash = require('connect-flash')
     const bodyParser = require('body-parser')
     const app = express()
     const mongoose = require('mongoose')
@@ -9,6 +11,21 @@
     const PORT = 8081
 
 // Configurações
+    app.use(session({
+        secret: 'cursodenode',
+        resave: true,
+        saveUninitialized: true
+    }))
+
+    app.use(flash())
+
+    app.use((req, res, next) => {
+        res.locals.success_msg = req.flash('success_msg')
+        res.locals.error_msg = req.flash('error_msg')
+
+        next()
+    })
+
     app.use(bodyParser.urlencoded({
         extended: true
     }))
@@ -26,9 +43,12 @@
         console.log('EErro ao tentar conexão com MongoDB!', err)
     });
 
-
-
     app.use(express.static(path.join(__dirname, 'public')))
+
+    // app.use((req, res, next) => {
+    //     console.log('Middleware!');
+    //     next()
+    // })
 // Rotas
     app.use('/admin', admin)
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+require('../models/Category')
+const Category = mongoose.model('categorias')
 
 router.get('/', (req, res) => {
     res.render('admin/index')
@@ -10,11 +13,134 @@ router.get('/posts', (req, res) => {
 })
 
 router.get('/categories', (req, res) => {
-    res.render('admin/categories')
+    Category.find().sort({
+        date: 'desc'
+    }).lean().then((categorias) => {
+        console.log('Categorias', categorias)
+        res.render('admin/categories', {
+            categories: categorias
+        })
+    }).catch((err) => {
+        req.flash('error_msg', "Erro ao listar Categorias!")
+        console.log('Erro ao listar categorias', err)
+        res.redirect('/admin')
+    })
 })
 
 router.get('/categories/add', (req, res) => {
     res.render('admin/addcategories')
+})
+
+router.post('/categories/new', (req, res) => {
+    var erros = []
+
+    if (!req.body.nome && typeof req.body.nome == undefined || req.body.nome == null) {
+        erros.push({texto: "Nome inválido!"})
+    }
+
+    if (!req.body.slug && typeof req.body.slug == undefined || req.body.slug == null) {
+        erros.push({texto: "Slug inválido!"})
+    }
+
+    if (req.body.nome.length < 2) {
+        erros.push({texto: "Nome da Categoria é muito pequeno!"})
+    }
+
+    if (erros.length > 0) {
+        console.log('ERROS ARR', erros)
+        res.render('admin/addcategories', {
+            erros: erros
+        })
+    } else {
+        const newCategory = {
+            nome: req.body.nome,
+            slug: req.body.slug
+        }
+
+        new Category(newCategory).save().then(() => {
+            console.log('Categoria salva')
+
+            req.flash("success_msg", "Categoria criada com sucesso!")
+
+            res.redirect('/admin/categories')
+        }).catch((err) => {
+            console.log('ERRO ao salvar categoria ', err)
+            req.flash("error_msg", "Erro ao salvar Categoria!")
+            res.redirect('/admin')
+        })
+
+    }
+
+});
+
+router.get('/categories/edit/:id', (req, res) => {
+    Category.findOne({
+        _id: req.params.id
+    }).lean().then((categoria) => {
+        res.render('admin/editcategory', {
+            category: categoria
+        })
+    }).catch((err) => {
+        console.log('Erro ao editar categoria', err)
+        req.flash('erro_msg', 'Categoria não encontrada!')
+        res.redirect('/admin/categories')
+    })
+})
+
+router.post('/categories/edit', (req, res) => {
+    var erros = []
+
+    if (!req.body.nome && typeof req.body.nome == undefined || req.body.nome == null) {
+        erros.push({texto: "Nome inválido!"})
+    }
+
+    if (!req.body.slug && typeof req.body.slug == undefined || req.body.slug == null) {
+        erros.push({texto: "Slug inválido!"})
+    }
+
+    if (req.body.nome.length < 2) {
+        erros.push({texto: "Nome da Categoria é muito pequeno!"})
+    }
+
+    if (erros.length > 0) {
+        console.log('ERROS ARR', erros)
+        res.render('admin/addcategories', {
+            erros: erros
+        })
+    } else {
+        Category.findOne({
+            _id: req.body.id
+        }).then((categoria) => {
+            categoria.nome = req.body.nome
+            categoria.slug = req.body.slug
+
+            categoria.save().then(() => {
+                req.flash('success_msg', 'Categoria atualizada com sucesso!')
+                res.redirect('/admin/categories')
+            }).catch((err) => {
+                req.flash('error_msg', 'Erro ao atualizar categoria!')
+                console.log('Erro ao atualizar categoria', err)
+                res.redirect('/admin/categories')
+            })
+
+        }).catch((err) => {
+            req.flash('error_msg', 'Erro ao atualizar categoria!')
+            res.redirect('/admin/categories')
+        })
+    }
+})
+
+router.post('/categories/delete', (req, res) => {
+    Category.findByIdAndDelete({
+        _id: req.body.id
+    }).then(() => {
+        req.flash('success_msg', 'Categoria deletada com sucesso!')
+        res.redirect('/admin/categories')
+    }).catch((err) => {
+        req.flash('error_msg', 'Erro ao deletar Categoria!')
+        console.log('Erro ao deletar categoria', err)
+        res.redirect('/admin/categories')
+    })
 })
 
 module.exports = router;
