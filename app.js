92368b959778028115a363c3bd814532ef84d9f5
Gustavo Felicio <gustavo.a.felicio@gmail.com>
2026-03-03 17:43:43 -0300

    categories and posts handling

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 // Módulos
+    require('./models/Post')
+    require('./models/Category')
     const express = require('express')
     const { engine } = require('express-handlebars')
     const session = require('express-session')
@@ -9,6 +11,9 @@
     const admin = require('./routes/admin')
     const path = require('path')
     const PORT = 8081
+
+    const Post = mongoose.model('postagens')
+    const Category = mongoose.model('categorias')
 
 // Configurações
     app.use(session({
@@ -58,7 +63,102 @@
     //     console.log('Middleware!');
     //     next()
     // })
-// Rotas
+// Rotas´
+    app.get('/', (req, res) => {
+        Post.find()
+            .populate('categoria')
+            .sort({data: 'desc'})
+            .lean()
+            .then((postagens) => {
+                res.render('index', {
+                    posts: postagens
+                })
+            }).catch((err) => {
+                req.flash('error_msg', 'Erro interno!')
+                console.log('Erro ao listar Postagens em index', err)
+                res.redirect('/404')
+            })
+    })
+
+    app.get('/post/:slug', (req, res) => {
+        Post.findOne({
+            slug: req.params.slug
+        })
+            .populate('categoria')
+            .lean()
+            .then((postagem) => {
+                if (postagem) {
+                    res.render('post/index', {
+                        post: postagem
+                    })
+                } else {
+                    req.flash('error_msg', 'Postagem não encontrada!')
+                    res.redirect('/')
+                }
+            })
+            .catch((err) => {
+                req.flash('error_msg', 'Postagem não encontrada!')
+                console.log('Erro ao listar Postagem', err)
+                res.redirect('/')
+            })
+    });
+
+    app.get('/categories', (req, res) => {
+        Category.find()
+            .lean()
+            .then((categorias) => {
+                res.render('categories/index', {
+                    categories: categorias
+                })
+            })
+            .catch((err) => {
+                req.flash('error_msg', 'Erro ao listar Categorias!')
+                console.log('Erro ao listar Categories', err)
+                res.redirect('/')
+            })
+    })
+
+    app.get('/categories/:slug', (req, res) => {
+        Category.findOne({
+            slug: req.params.slug
+        })
+            .lean()
+            .then((categoria) => {
+                if (categoria) {
+                    Post.find({
+                        categoria: categoria._id
+                    })
+                        .sort({
+                            data: 'desc'
+                        })
+                        .lean()
+                        .then((postagens) => {
+                            res.render('categories/posts', {
+                                category: categoria,
+                                posts: postagens
+                            })
+                        })
+                        .catch((err) => {
+                            req.flash('error_msg', 'Erro ao listar Postagens!')
+                            console.log('Erro ao listar POSTAGENS', err)
+                            res.redirect('/categories')
+                        })
+                } else {
+                    req.flash('error_msg', 'Categoria não encontrada!')
+                    res.redirect('/categories')
+                }
+            })
+            .catch((err) => {
+                req.flash('error_msg', 'Erro ao recuperar Categoria!')
+                console.log('Erro ao recuperar Categoria', err)
+                res.redirect('/categories')
+            })
+    })
+
+    app.get('/404', (req, res) => {
+        res.send('Erro 404!')
+    })
+
     app.use('/admin', admin)
 
 // Outros
